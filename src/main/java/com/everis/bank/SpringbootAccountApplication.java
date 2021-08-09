d3dc0e71e908eab36e981721007d78cd139b27df
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-09 16:16:54 -0500

    feat: Added config server config

--- src/main/java/com/everis/bank/SpringbootAccountApplication.java
+++ src/main/java/com/everis/bank/SpringbootAccountApplication.java
@@ -3,12 +3,9 @@ package com.everis.bank;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//
-//@EnableCircuitBreaker
-//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbootAccountApplication implements CommandLineRunner {
 
